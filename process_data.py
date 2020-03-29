1c5a83531abf861979bad5203438aa7c1cdb4835
Ian <ihafkenschiel@gmail.com>
2020-03-29 00:04:47 -0300

    Labels almost readable

--- process_data.py
+++ process_data.py
@@ -31,8 +31,29 @@ with open(INPUT_FILE) as csv_file:
     print(headers)
     print(us_data)
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
+'''
+fig, ax = plt.subplots()
 ax.plot(headers,us_data)
+ax.set_xlim(0, 20)
+ax.set_xlabel('2020')
+ax.set_ylabel('Confirmed Cases')
 
 fig.savefig('covidcases-US.png')
+'''
+
+x = headers
+y = us_data
+plt.figure()
+plt.plot(x, y)
+plt.bar(x, y, alpha=0.2)
+plt.title("US Confirmed Cases of Covid-19")
+plt.xlabel('2020')
+plt.ylabel('Confirmed Cases')
+plt.xticks(x, [str(i) for i in x], rotation=90)
+
+#set parameters for tick labels
+plt.tick_params(axis='x', which='major', labelsize=5)
+
+#plt.tight_layout()
+
+plt.savefig('covidcases-US.png')
